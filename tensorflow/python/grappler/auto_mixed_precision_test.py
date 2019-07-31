4089abf5c409bc37fc8cfeb73b5351b3a7ef6eca
Kaixi Hou <kaixih@nvidia.com>
2019-07-31 11:13:54 -0700

    change the reshape part to more realistic scenario

--- tensorflow/python/grappler/auto_mixed_precision_test.py
+++ tensorflow/python/grappler/auto_mixed_precision_test.py
@@ -97,13 +97,14 @@ def _conv_bn(x):
 
 def _conv3d_bn(x):
   """Conv3D followed by batchnorm."""
-  i = array_ops.reshape(x, [-1, 8, 8, 8, 1])
-  f = _weight([3, 3, 3, 1, 6])
-  x = _conv3d(i, f)
+  in_ch = x.get_shape().as_list()[-1]
+  f = _weight([3, 3, 3, in_ch, 6])
+  x = _conv3d(x, f)
   s = _weight([6])
   o = _weight([6])
   x = array_ops.reshape(x, [-1, 8, 8, 6])
   y, _, _ = _fused_batchnorm(x, s, o)
+  y = array_ops.reshape(y, [-1, 8, 8, 8, 6])
   y = array_ops.identity(y)
   return y
 

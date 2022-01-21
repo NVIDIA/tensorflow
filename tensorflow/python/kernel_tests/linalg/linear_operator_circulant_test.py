050dd148d77ce71c39d0845419152c413c830fc7
Nathan Luehr <nluehr@nvidia.com>
2022-01-21 15:55:24 -0600

    Increase tol for ARM in LinearOperatorCirculant2DTestNonHermitianSpectrum.test_real_hermitian_spectrum_gives_real_symmetric_operator

--- tensorflow/python/kernel_tests/linalg/linear_operator_circulant_test.py
+++ tensorflow/python/kernel_tests/linalg/linear_operator_circulant_test.py
@@ -575,7 +575,7 @@ class LinearOperatorCirculant2DTestNonHermitianSpectrum(
           [matrix_tensor, matrix_t, imag_matrix])
 
       np.testing.assert_allclose(0, imag_matrix, atol=1e-6)
-      self.assertAllClose(matrix, matrix_transpose, atol=0)
+      self.assertAllClose(matrix, matrix_transpose, atol=0, rtol=1e-5)
 
   @test_util.run_v1_only("b/120545219")
   def test_real_spectrum_gives_self_adjoint_operator(self):
